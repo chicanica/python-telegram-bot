dc6b28918261c5ae68c97c58876f184edcb820c0
student <www.kopina2705@mail.ru>
2022-02-25 16:22:14 +0300

    added menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Вариант 1 - самый простой чат бот, просто отзывается
 
 import telebot  # pyTelegramBotAPI	4.3.1
+from telebot import types
 
 bot = telebot.TeleBot('5193117811:AAH0hWHVx0kH08sub52IFj2SAdJi1eugY-k')  # Создаем экземпляр бота @Ivanov_Ivan_1MD19_bot
 
@@ -9,6 +10,11 @@ bot = telebot.TeleBot('5193117811:AAH0hWHVx0kH08sub52IFj2SAdJi1eugY-k')  # Со�
 @bot.message_handler(commands=["start"])
 def start(message, res=False):
     chat_id = message.chat.id
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Главное меню")
+    btn2 = types.KeyboardButton("❓ Помощь")
+    markup.add(btn1, btn2)
 
     bot.send_message(chat_id,
                      text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
@@ -21,8 +27,46 @@ def start(message, res=False):
 def get_text_messages(message):
     chat_id = message.chat.id
     ms_text = message.text
-    bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
 
+    if ms_text == "Главное меню" or ms_text == "👋 Главное меню" or ms_text == "Вернуться в главное меню":  # ..........
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Развлечения")
+        btn2 = types.KeyboardButton("WEB-камера")
+        btn3 = types.KeyboardButton("Управление")
+        back = types.KeyboardButton("Помощь")
+        markup.add(btn1, btn2, btn3, back)
+        bot.send_message(chat_id, text="Вы в главном меню", reply_markup=markup)
+
+    elif ms_text == "Развлечения":  # ..................................................................................
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Прислать собаку")
+        btn2 = types.KeyboardButton("Прислать анекдот")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, back)
+        bot.send_message(chat_id, text="Развлечения", reply_markup=markup)
+
+    elif ms_text == "/dog" or ms_text == "Прислать собаку":  # .........................................................
+        bot.send_message(chat_id, text="еще не готово...")
+
+    elif ms_text == "Прислать анекдот":  # .............................................................................
+        bot.send_message(chat_id, text="еще не готово...")
+
+    elif ms_text == "WEB-камера":
+        bot.send_message(chat_id, text="еще не готово...")
+
+    elif ms_text == "Управление":  # ...................................................................................
+        bot.send_message(chat_id, text="еще не готово...")
+
+    elif ms_text == "Помощь" or ms_text == "/help":  # .................................................................
+        bot.send_message(chat_id, "Автор: Швец Андрей")
+        key1 = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text="Напишите автору", url="https://t.me/user59387")
+        key1.add(btn1)
+        img = open('Швец Андрей.png', 'rb')
+        bot.send_photo(message.chat.id, img, reply_markup=key1)
+
+    else:  # ...........................................................................................................
+        bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
 
 # -----------------------------------------------------------------------
 bot.polling(none_stop=True, interval=0) # Запускаем бота
